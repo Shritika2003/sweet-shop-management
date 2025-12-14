92885271ddbc3658e7545773493c7acd34ce9105
Shritika Sood <soodshritika9@gmail.com>
2025-12-14 20:38:39 +0530

    feat: add React frontend and connect with FastAPI backend

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+from fastapi.middleware.cors import CORSMiddleware
+
 from fastapi import FastAPI
 from app.database import Base, engine
 from app.routers import auth, sweets, inventory
@@ -6,6 +8,14 @@ from app.routers import auth, sweets, inventory
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Sweet Shop Management System")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3001"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 # register routers
 app.include_router(auth.router)
